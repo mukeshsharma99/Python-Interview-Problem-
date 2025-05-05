fbae10aaa324882b1e92b28e84544b5348a8304b
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-05 23:44:22 +0530

    push

---
+++ Problem_4.py
@@ -0,0 +1,10 @@
+# Write a python program to find the give numer positive or negatiev using loop
+
+for i in range(10):
+    num  = int(input('Enter a number :'))
+    if num > 0 :
+        print('Positive')
+    elif num == 0:
+        print('Zero')
+    else:
+        print('Negative ')
