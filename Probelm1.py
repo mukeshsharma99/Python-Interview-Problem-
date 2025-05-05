21ebef65471e83b4a2fc3ebd1f45e5b0dc712f40
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-05 20:23:00 +0530

    push

--- Probelm1.py
+++ Probelm1.py
@@ -1,1 +1,9 @@
-print('Hi Mr. Mukesh kumar how are you doing today ')
+# Write a pytyhon program to find the given number odd or even 
+
+num = int(input('Enter a number: '))
+
+if num%2 == 0:
+    print('Given number is odd', num)
+
+else:
+    print('Given number is even', num)
