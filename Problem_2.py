99063408911837321abbfdd5dad7101e3fa73d37
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-05 20:36:40 +0530

    push

---
+++ Problem_2.py
@@ -0,0 +1,8 @@
+# Write a python program to find the odd and even using loop
+
+for i in range(10):
+    num = int(input('Enter  a  number :'))
+    if num% 2 ==0:
+        print('Given number is odd: ')
+    else:
+        print('Given number is Even : ')
