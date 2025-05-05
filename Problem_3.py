60b74edc2512f8437a8c5f7d36b7e4f0854fb81f
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-05 23:36:41 +0530

    Push

---
+++ Problem_3.py
@@ -0,0 +1,10 @@
+# Write a python program to find a  given number positive or negative 
+
+num = int(input('Enter a number'))
+
+if num > 0:
+    print('Positive')
+elif num == 0:
+    print('Zero')
+else:
+    print('Negative')
