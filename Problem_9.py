c6b3750d115bdf10290610c260ada467758276ab
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-07 20:34:41 +0530

    push

--- Problem_9.py
+++ Problem_9.py
@@ -1,7 +1,15 @@
 # Write a python program to find the given numebr palindrome or not 
 
-num = int(input('enter the number'))
+num = int(input('enter the number: '))
 
 temp  = num
 reverse  = 0
 
+while temp> 0:
+    remainder = temp % 10
+    reverse = ( reverse *10) + remainder
+    temp = temp// 10
+if num == reverse:
+    print('Palindrome')
+else:
+    print('Not Palindorme') 
