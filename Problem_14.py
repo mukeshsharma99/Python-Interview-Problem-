b34d8c950a8455bed0163af3741a4b3d2a761684
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-20 21:58:07 +0530

    Push

--- Problem_14.py
+++ Problem_14.py
@@ -2,4 +2,12 @@
 
 def maximum(a,b,c):
     if (a >= b) and (a>=c):
-        return
+        largest = a
+    elif (b >=a) and (b>=c):
+        largest = b
+    else:
+        largest = c
+
+    return largest
+
+hdf78ikjh,mnbfdt9yrdojln;, 
