2963749bf0d391738c86f26e9e3134ed83d39cff
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-20 23:09:28 +0530

    push

---
+++ Problem_17.py
@@ -0,0 +1,12 @@
+# Write a python program to findthe factorial number  using  Recursive Approach:
+
+def fac_recursive(n):
+    if n <0:
+        return 'Facorial not define for nagetive number'
+    
+    if n ==0 or n== 1:
+        return 1
+    return n* fac_recursive (n -1)
+
+num = int(input('Enter a number: '))
+print(fac_recursive(num))
