e7bbe71f753c462a483d3e57b23350c9d26ea7fe
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-20 20:47:27 +0530

    push

---
+++ Problem_13.py
@@ -0,0 +1,12 @@
+# Write a program to find a minimum of two numbers.
+
+
+def minimum(a,b):
+    if a<=b:
+        return a
+    else:
+        return b
+    
+a = int(input('Enter a number: '))
+b =  int(input('Enter a number: '))
+print(minimum(a, b))
