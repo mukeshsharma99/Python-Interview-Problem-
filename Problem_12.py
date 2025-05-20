ab844cc70f0cd0722a914244b8e45cfe2caec861
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-20 20:00:08 +0530

    push

--- Problem_12.py
+++ Problem_12.py
@@ -1,1 +1,14 @@
 # Write a program to find a maximum of two numbers.
+def maximum(a, b):
+    if a>=b:
+        return a
+    else:
+        return b
+    
+
+a =  int(input('enter the number :'))
+b  = int(input('Enter the number:'))
+
+print(maximum(a,b))
+
+
