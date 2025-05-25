4c502edb867e8aae6c4f3cfd2652aa7b5bb25a85
mukeshsharma99 <mukesh10400@gmail.com>
2025-05-25 23:50:58 +0530

    push

--- Problem_19.py
+++ Problem_19.py
@@ -1,15 +1,22 @@
 # Write the python program to find the gcd
 
-def gcd(a,b):
+def gcd(a, b):
 
-
-    if a == 0:
+    if (a == 0):
         return b
-    if b == 0:
+    if (b == 0):
         return a
-    if (a==b):
+
+    if (a == b):
         return a
-    if (a>b):
-        return gcd(a-b,b)
+
+    if (a > b):
+        return gcd(a-b, b)
+    return gcd(a, b-a)
+
 a = 98
-    
+b = 56
+if(gcd(a, b)):
+    print('GCD of', a, 'and', b, 'is', gcd(a, b))
+else:
+    print('not found')
--- Problem_20.py
+++
@@ -1,1 +0,0 @@
-ieugiorbhiuntioj bdio
